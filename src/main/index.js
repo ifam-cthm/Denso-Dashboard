34d289d377280173a25a64fd88f6d5c4c565f566
Jonathas Santos <jonathas.santos@meliuz.com.br>
2019-06-06 14:10:10 -0400

    fix: Adequacoes nas janelas e componentes

--- src/main/index.js
+++ src/main/index.js
@@ -18,9 +18,7 @@ function createWindow() {
    * Initial window options
    */
   mainWindow = new BrowserWindow({
-    height: 563,
-    useContentSize: true,
-    width: 1000
+    useContentSize: true
   })
 
   mainWindow.loadURL(winURL)
